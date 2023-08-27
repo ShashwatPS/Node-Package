e038b9eac1e4837de4424df54afcce9fc6902f08
Shashwat Singh <shashwat123student@gmail.com>
2023-08-27 22:41:51 +0530

    Updated Package Version

--- dist/Practice.js
+++ dist/Practice.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@shashwat_ps/common");
+import common_1 from "@shashwat_ps/common";
 let username = "Shashwat Singh";
 let password = 12345;
 let parsedInput = common_1.signupInput.safeParse({ username, password });
